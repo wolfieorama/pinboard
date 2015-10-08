8a15bc900327cff1c3c36aaee27b75981ac3f0af
wolfieorama <wachirawamunyi@gmail.com>
2015-10-08 23:20:32 +0300

    added user ascoaition and user eail on show

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -14,7 +14,7 @@ class PinsController < ApplicationController
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   # GET /pins/1/edit
@@ -24,7 +24,7 @@ class PinsController < ApplicationController
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     respond_to do |format|
       if @pin.save
