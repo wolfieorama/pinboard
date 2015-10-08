fdb06363ed511284ab768b30f85e9a4eeb255d23
wolfieorama <wachirawamunyi@gmail.com>
2015-10-08 23:00:12 +0300

    added active record association

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004230427) do
+ActiveRecord::Schema.define(version: 20151008194910) do
 
   create_table "pins", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
